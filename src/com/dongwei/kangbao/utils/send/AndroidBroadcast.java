e34fdefa0728bf4ed1e49c2ecac46682a2373f66
zhj0921 <623105334@qq.com>
2017-02-27 14:22:58 +0800

    Revert "dw_live"
    This reverts commit 0abd78cdb0e3b74d9f8b6166b58584ff7aa757c7.

--- src/com/dongwei/kangbao/utils/send/AndroidBroadcast.java
+++
@@ -1,9 +0,0 @@
-package com.dongwei.kangbao.utils.send;
-
-public class AndroidBroadcast extends AndroidNotification {
-	public AndroidBroadcast(String appkey,String appMasterSecret) throws Exception {
-			setAppMasterSecret(appMasterSecret);
-			setPredefinedKeyValue("appkey", appkey);
-			this.setPredefinedKeyValue("type", "broadcast");	
-	}
-}
